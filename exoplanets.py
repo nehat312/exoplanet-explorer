e66ced10366730dace65ccfbb05353fe19fe2273
nehat312 <nate.ehat@gmail.com>
2022-08-07 22:10:34 -0400

    Update exoplanets.py

--- exoplanets.py
+++ exoplanets.py
@@ -33,35 +33,80 @@ from PIL import Image
 # pd.set_option('display.max_colwidth', 200)
 
 
-## DATA IMPORTS ##
-
+## DIRECTORY CONFIGURATION ##
+abs_path = r'https://raw.githubusercontent.com/nehat312/exoplanet-explorer/main'
+exoplanet_path = abs_path + '/data/NASA_Exoplanets-8-7-22.csv'
 engine_csv = r'https://raw.githubusercontent.com/nehat312/prop-swap/main/data/engine.csv'
-all_investor_idx = pd.read_csv(engine_csv) #, header=0, index_col=0 #, encoding='utf-8'
 
-abspath_curr = ''
+## DATA IMPORT ##
+exoplanets = pd.read_csv(exoplanet_path, header=0, index_col='loc_rowid') #, header=0, index_col='pl_name'#,
 
-planetary_systems = abspath_curr + '/data/NASA_Planetary_Systems-4-24-22'
-exoplanets = abspath_curr + '/data/NASA_Exoplanets-4-22-22'
+#%%
+# print(exoplanets.info())
+# exoplanets.dropna(inplace=True)
 
-#planetary_systems = pd.read_csv(planetary_systems + '.csv', index_col='pl_name')
-#exoplanets = pd.read_csv(exoplanets + '.csv', index_col='pl_name')
+print(exoplanets.info())
+print(exoplanets.columns)
+# print(exoplanets.head())
 
-## VARIABLE ASSIGNMENT
+#%%
+## VARIABLE ASSIGNMENT ##
+## USED FOR MODELING
+exoplanet_num_cols = exoplanets[['sy_star_count', 'sy_planet_count',
+                                  'pl_orbper', 'pl_orbsmax', 'pl_rade', 'pl_radj',
+                                 'pl_bmasse', 'pl_bmassj', 'pl_bmassprov', 'pl_orbeccen',
+                                 'ttv_flag', 'st_temp_eff_k', 'st_temp_eff_k1', 'st_temp_eff_k2', 'st_radius', 'st_mass',
+                                  'st_metallicity', 'st_surf_gravity', 'rastr', 'ra', 'decstr', 'dec',
+                                  'sy_distance_pc', 'sy_vmag', 'sy_kmag', 'sy_gaiamag'
+                                 ]]
 
-all_investor_idx = all_investor_idx.sort_values(by='TTL_VOL_RANK')
+## USED FOR VISUALIZATION
+exoplanets_all_cols = exoplanets[['loc_rowid',
+                                  'host_name', 'sy_star_count', 'sy_planet_count',
+                                  'disc_method', 'disc_year', 'disc_facility',
+                                  'disc_telescope', 'disc_instrument', 'pl_controv_flag', 'pl_orbper',
+                                  'pl_orbsmax', 'pl_rade', 'pl_radj', 'pl_bmasse', 'pl_bmassj',
+                                  'pl_bmassprov', 'pl_orbeccen', 'ttv_flag', 'st_temp_eff_k',
+                                  'st_temp_eff_k1', 'st_temp_eff_k2', 'st_radius', 'st_mass',
+                                  'st_metallicity', 'st_surf_gravity', 'rastr', 'ra', 'decstr', 'dec',
+                                  'sy_distance_pc', 'sy_vmag', 'sy_kmag', 'sy_gaiamag'
+                                  ]]
 
-investor_cols = ['INVESTOR', 'INVESTOR_TYPE', 'CITY', 'STATE', 'COUNTRY', 'MSA', 'WEBSITE', 'C-SUITE']
-mf_cols = ['INVESTOR', 'INVESTOR_TYPE', 'MF_AVG_PRICE_MM', 'MF_UNITS_PROP', 'MF_AVG_PPU',  'MF_QUALITY', 'MF_VOL_RANK', 'CITY', 'STATE', 'COUNTRY', 'MSA', 'WEBSITE'] # AVG_QUALITY, 'TTL_VOL_RANK', 'TTL_SF_RANK',
-sc_cols = ['INVESTOR', 'INVESTOR_TYPE', 'SC_AVG_PRICE_MM', 'SC_SF_PROP', 'SC_AVG_PSF',  'SC_QUALITY', 'SC_VOL_RANK', 'CITY', 'STATE', 'COUNTRY', 'MSA', 'WEBSITE']
-nnn_cols = ['INVESTOR', 'INVESTOR_TYPE', 'NNN_AVG_PRICE_MM', 'NNN_SF_PROP', 'NNN_AVG_PSF',  'NNN_QUALITY', 'NNN_VOL_RANK', 'CITY', 'STATE', 'COUNTRY', 'MSA', 'WEBSITE']
-mall_cols = ['INVESTOR', 'INVESTOR_TYPE', 'MALL_AVG_PRICE_MM', 'MALL_SF_PROP', 'MALL_AVG_PSF',  'MALL_QUALITY', 'MALL_VOL_RANK', 'CITY', 'STATE', 'COUNTRY', 'MSA', 'WEBSITE']
-ss_cols = ['INVESTOR', 'INVESTOR_TYPE', 'SS_AVG_PRICE_MM', 'SS_SF_PROP',  'SS_AVG_PSF',  'SS_QUALITY', 'SS_VOL_RANK', 'CITY', 'STATE', 'COUNTRY', 'MSA', 'WEBSITE']
-ind_cols = ['INVESTOR', 'INVESTOR_TYPE', 'IND_AVG_PRICE_MM', 'IND_SF_PROP', 'IND_AVG_PSF',  'IND_QUALITY', 'IND_VOL_RANK', 'CITY', 'STATE', 'COUNTRY', 'MSA', 'WEBSITE', ]
-fs_cols = ['INVESTOR', 'INVESTOR_TYPE', 'FS_AVG_PRICE_MM', 'FS_KEYS_PROP', 'FS_AVG_PPK',  'FS_QUALITY', 'FS_VOL_RANK', 'CITY', 'STATE', 'COUNTRY', 'MSA', 'WEBSITE', ]
-ls_cols = ['INVESTOR', 'INVESTOR_TYPE', 'LS_AVG_PRICE_MM', 'LS_KEYS_PROP', 'LS_AVG_PPK',  'LS_QUALITY', 'LS_VOL_RANK', 'CITY', 'STATE', 'COUNTRY', 'MSA', 'WEBSITE', ]
-cbd_cols = ['INVESTOR', 'INVESTOR_TYPE', 'CBD_AVG_PRICE_MM', 'CBD_SF_PROP', 'CBD_AVG_PSF',  'CBD_QUALITY', 'CBD_VOL_RANK', 'CITY', 'STATE', 'COUNTRY', 'MSA', 'WEBSITE', ]
-sub_cols = ['INVESTOR', 'INVESTOR_TYPE', 'SUB_AVG_PRICE_MM', 'SUB_SF_PROP', 'SUB_AVG_PSF',  'SUB_QUALITY', 'SUB_VOL_RANK', 'CITY', 'STATE', 'COUNTRY', 'MSA', 'WEBSITE', ]
+## STYLE DICTIONARY ##
+YlOrRd = px.colors.sequential.YlOrRd
+Mint = px.colors.sequential.Mint
+Electric = px.colors.sequential.Electric
+Sunsetdark = px.colors.sequential.Sunsetdark
+Sunset = px.colors.sequential.Sunset
+Tropic = px.colors.diverging.Tropic
 
+chart_labels = {'pl_name':'PLANET NAME',
+                'host_name':'HOST NAME',
+                'sy_star_count':'SYSTEM STARS',
+                'sy_planet_count':'SYSTEM PLANETS',
+                'disc_method':'DISCOVERY METHOD',
+                'disc_year':'DISCOVERY YEAR',
+                'disc_facility':'DISCOVERY FACILITY',
+                'disc_telescope':'DISCOVERY TELESCOPE',
+                'disc_instrument':'DISCOVERY INSTRUMENT',
+                'pl_orbper':'PLANET ORBITAL PERIOD',
+                # 'pl_orbsmax':'PLANET ORBITAL',
+                'pl_rade':'PLANET RAD (E)',
+                'pl_radj':'PLANET RAD (J)',
+                'pl_bmasse':'PLANET MASS (E)',
+                'pl_bmassj':'PLANET MASS (J)',
+                'pl_orbeccen':'PLANET ORBITAL ECCENTRICITY',
+                'st_temp_eff_k':'STAR TEMPERATURE (K)',
+                'st_radius':'STAR RADIUS',
+                'st_mass':'STAR MASS',
+                'st_metallicity':'STAR METALLICITY',
+                'st_surf_gravity':'STAR SURFACE GRAVITY',
+                'sy_distance_pc':'STAR DISTANCE (PC)',
+                'ra':'RIGHT ASCENSION',
+                'dec':'DECLINATION',
+                'glon':'GALACTIC LONGITUDE',
+                'glat':'GALACTIC LATITUDE'
+                }
 
 # #%%
 
@@ -86,52 +131,66 @@ st.markdown(hide_menu_style, unsafe_allow_html=True)
 st.container()
 
 st.title('EXOPLANET EXPLORER')
-st.subheader('*NASA-CalTECH Archives*')
+st.subheader('*Data sourced from NASA-CalTECH mission archives*')
 
-prop_params_header = st.header('INPUT PARAMETERS:')
+exo_chart_1 = px.scatter(exoplanets, #all_investor_idx
+                                    x=exoplanets['st_rad'],
+                                    y=exoplanets['st_mass'],
+                                    color=exoplanets['st_temp_eff_k'],
+                                    color_continuous_scale=Sunsetdark, #'YlOrRd', #'Tropic',
+                                    color_discrete_sequence=Sunsetdark,
+                                    hover_name=exoplanets['host_name'],
+                                    # hover_data=exoplanets[['sy_star_count', 'sy_planet_count']],
+                                    title='HOST STAR ATTRIBUTES',
+                                    labels=chart_labels,
+                                    )
 
-sector = st.selectbox('PARAMS:',
-                      ('TBU',
-                       'TBU'
-                       'TBU'
-                       'TBU'
-                       'TBU')
-                      )
+st.plotly_chart(exo_chart_1, use_container_width=False, sharing="streamlit")
+
+# prop_params_header = st.header('INPUT PARAMETERS:')
+#
+# exoplanet_list = st.selectbox('EXOPLANETS:',
+#                       ('TBU',
+#                        'TBU'
+#                        'TBU'
+#                        'TBU'
+#                        'TBU')
+#                       )
 
 # st.sidebar.xyz
 
-with st.form('PARAMS FORM'):
-    if sector == "MULTIFAMILY":
-        prop_size = st.number_input('*TOTAL MF UNITS [25-1,000 UNITS]', min_value=25, max_value=500, step=25, value=100) #list(range(25,750,25)))
-        min_prop_price = st.number_input('*MINIMUM VALUATION [$0MM-$100MM]', min_value=0, max_value=100, value=10, step=5)
-        prop_qual = st.selectbox('*PROPERTY QUALITY [1-5]:', list(range(1, 6, 1)))
-
-
-    params_submit = st.form_submit_button('FORM SUBMIT')
-
-    @st.cache(persist=True, allow_output_mutation=True)
-    def filter_buyers(sector, prop_size, min_prop_price, prop_qual):
-      if sector == 'MULTIFAMILY':
-        for investors in all_investor_idx:
-          mf_size_filter = all_investor_idx[all_investor_idx.MF_UNITS_PROP >= prop_size]
-          mf_min_price_filter = mf_size_filter[mf_size_filter.MF_AVG_PRICE_MM >= min_prop_price]
-          mf_qual_filter = mf_min_price_filter[(mf_min_price_filter.MF_QUALITY >= (prop_qual-1)) & (mf_min_price_filter.MF_QUALITY <= (prop_qual+1))]
-          mf_buyer_recs = mf_qual_filter.sort_values(by = 'MF_VOL_RANK', ascending = True)[:50]
-          mf_buyer_recs = pd.DataFrame(data = mf_buyer_recs, columns = mf_cols)
-        return mf_buyer_recs
-      elif sector == 'STRIP CENTER':
-        for investors in all_investor_idx:
-          sc_size_filter = all_investor_idx[all_investor_idx['SC_SF_PROP'] >= prop_size]
-          sc_min_price_filter = sc_size_filter[sc_size_filter['SC_AVG_PRICE_MM'] >= min_prop_price]
-          sc_qual_filter = sc_min_price_filter[(sc_min_price_filter['SC_QUALITY'] >= (prop_qual-1)) & (sc_min_price_filter['SC_QUALITY'] <= (prop_qual+1))]
-          sc_buyer_recs = sc_qual_filter.sort_values(by = 'SC_VOL_RANK', ascending = True)[:50]
-          sc_buyer_recs = pd.DataFrame(data = sc_buyer_recs, columns = sc_cols)
-        return sc_buyer_recs
-
-
-## TARGET INVESTOR DATAFRAME ##
-    if params_submit:
-        buyer_rec_df = filter_buyers(sector, prop_size, min_prop_price, prop_qual)
+# with st.form('PARAMS FORM'):
+#     if sector == "MULTIFAMILY":
+#         prop_size = st.number_input('*TOTAL MF UNITS [25-1,000 UNITS]', min_value=25, max_value=500, step=25, value=100) #list(range(25,750,25)))
+#         min_prop_price = st.number_input('*MINIMUM VALUATION [$0MM-$100MM]', min_value=0, max_value=100, value=10, step=5)
+#         prop_qual = st.selectbox('*PROPERTY QUALITY [1-5]:', list(range(1, 6, 1)))
+#
+#
+#     params_submit = st.form_submit_button('FORM SUBMIT')
+#
+#     @st.cache(persist=True, allow_output_mutation=True)
+#     def filter_buyers(sector, prop_size, min_prop_price, prop_qual):
+#       if sector == 'MULTIFAMILY':
+#         for investors in all_investor_idx:
+#           mf_size_filter = all_investor_idx[all_investor_idx.MF_UNITS_PROP >= prop_size]
+#           mf_min_price_filter = mf_size_filter[mf_size_filter.MF_AVG_PRICE_MM >= min_prop_price]
+#           mf_qual_filter = mf_min_price_filter[(mf_min_price_filter.MF_QUALITY >= (prop_qual-1)) & (mf_min_price_filter.MF_QUALITY <= (prop_qual+1))]
+#           mf_buyer_recs = mf_qual_filter.sort_values(by = 'MF_VOL_RANK', ascending = True)[:50]
+#           mf_buyer_recs = pd.DataFrame(data = mf_buyer_recs, columns = mf_cols)
+#         return mf_buyer_recs
+#       elif sector == 'STRIP CENTER':
+#         for investors in all_investor_idx:
+#           sc_size_filter = all_investor_idx[all_investor_idx['SC_SF_PROP'] >= prop_size]
+#           sc_min_price_filter = sc_size_filter[sc_size_filter['SC_AVG_PRICE_MM'] >= min_prop_price]
+#           sc_qual_filter = sc_min_price_filter[(sc_min_price_filter['SC_QUALITY'] >= (prop_qual-1)) & (sc_min_price_filter['SC_QUALITY'] <= (prop_qual+1))]
+#           sc_buyer_recs = sc_qual_filter.sort_values(by = 'SC_VOL_RANK', ascending = True)[:50]
+#           sc_buyer_recs = pd.DataFrame(data = sc_buyer_recs, columns = sc_cols)
+#         return sc_buyer_recs
+#
+#
+# ## USER PARAMS DATAFRAME ##
+#     if params_submit:
+#         buyer_rec_df = filter_buyers(sector, prop_size, min_prop_price, prop_qual)
 
         # st.write("TARGETED INVESTOR POOL:")
         # st.dataframe(buyer_rec_df)
@@ -148,79 +207,18 @@ with st.form('PARAMS FORM'):
         #
         # st.dataframe(buyer_rec_df.style.applymap(df_style_map, subset=['COUNTRY']))
 
-        ## STYLE DICTIONARY ##
 
-        YlOrRd = px.colors.sequential.YlOrRd
-        Mint = px.colors.sequential.Mint
-        Electric = px.colors.sequential.Electric
-        Sunsetdark = px.colors.sequential.Sunsetdark
-        Sunset = px.colors.sequential.Sunset
-        Tropic = px.colors.diverging.Tropic
-
-        chart_labels = {'INVESTOR_TYPE': 'INVESTOR TYPE',
-
-                        }
 
 
 ## VALUATION METRICS ##
-        if sector == 'MULTIFAMILY':
-            per_unit_valuation = round(buyer_rec_df['MF_AVG_PPU'].mean())
-            prop_valuation = per_unit_valuation * prop_size
-            st.write(f'ESTIMATED PROPERTY VALUATION: ${(prop_valuation / 1_000_000):.2f}MM or {per_unit_valuation:.0f}/UNIT')
-            # st.metric('ESTIMATED PROPERTY VALUATION: $', (prop_valuation / 1_000_000))
-            # st.metric('ESTIMATED PROPERTY VALUATION: $/UNIT', per_unit_valuation)
-            st.write("TARGETED INVESTOR POOL:")
-            st.dataframe(buyer_rec_df)
-
-            mf_chart_1 = px.scatter(buyer_rec_df, #all_investor_idx
-                                    x=buyer_rec_df['MF_AVG_PRICE_MM'],
-                                    y=buyer_rec_df['MF_AVG_PPU'],
-                                    # color=buyer_rec_df['INVESTOR_TYPE'],
-                                    color=buyer_rec_df['MF_AVG_PPU'],
-                                    color_continuous_scale=Sunsetdark, #'YlOrRd', #'Tropic',
-                                    color_discrete_sequence=Sunsetdark,
-                                    hover_name=buyer_rec_df['INVESTOR'],
-                                    hover_data=buyer_rec_df[['MSA']],
-                                    title='TARGETED INVESTOR POOL',
-                                    labels=chart_labels,
-                                    )
-
-            st.plotly_chart(mf_chart_1, use_container_width=False, sharing="streamlit")
-
-
-            mf_chart_2 = px.bar(buyer_rec_df,
-                                x=buyer_rec_df['INVESTOR'],
-                                y=buyer_rec_df['MF_AVG_PPU'],
-                                color=buyer_rec_df['INVESTOR_TYPE'],
-                                color_continuous_scale=Tropic,
-                                color_discrete_sequence=Tropic,
-                                # category_orders={'total descending'},
-                                hover_name=buyer_rec_df['INVESTOR'],
-                                hover_data=buyer_rec_df[['MSA']],
-                                labels=chart_labels,
-                                barmode='relative',
-                                # size=buyer_rec_df['SC_VOL_RANK'],
-                                height=500,
-                                # width=400,
-                                )
-
-            st.plotly_chart(mf_chart_2, use_container_width=False, sharing="streamlit")
-
-
-
-            #pattern_shape = "nation", pattern_shape_sequence = [".", "x", "+"]
-
-            # fig = px.bar(df, x="sex", y="total_bill", color="smoker", barmode="group", facet_row="time", facet_col="day",
-            #        category_orders={"day": ["Thur", "Fri", "Sat", "Sun"], "time": ["Lunch", "Dinner"]})
-
-            # fig = px.scatter_matrix(df, dimensions=["sepal_width", "sepal_length", "petal_width", "petal_length"], color="species")
-
-            # fig = px.parallel_categories(df, color="size", color_continuous_scale=px.colors.sequential.Inferno)
-
-            # fig = px.parallel_coordinates(df, color="species_id", labels={"species_id": "Species",
-            #                   "sepal_width": "Sepal Width", "sepal_length": "Sepal Length",
-            #                   "petal_width": "Petal Width", "petal_length": "Petal Length", },
-            #                     color_continuous_scale=px.colors.diverging.Tealrose, color_continuous_midpoint=2)
+        # if sector == 'MULTIFAMILY':
+        #     per_unit_valuation = round(buyer_rec_df['MF_AVG_PPU'].mean())
+        #     prop_valuation = per_unit_valuation * prop_size
+        #     st.write(f'ESTIMATED PROPERTY VALUATION: ${(prop_valuation / 1_000_000):.2f}MM or {per_unit_valuation:.0f}/UNIT')
+        #     # st.metric('ESTIMATED PROPERTY VALUATION: $', (prop_valuation / 1_000_000))
+        #     # st.metric('ESTIMATED PROPERTY VALUATION: $/UNIT', per_unit_valuation)
+        #     st.write("TARGETED INVESTOR POOL:")
+        #     st.dataframe(buyer_rec_df)
 
 
 #######################
@@ -308,6 +306,20 @@ st.stop()
 # style={'textAlign': 'Center', 'backgroundColor': 'rgb(223,187,133)',
 #                                            'color': 'black', 'fontWeight': 'bold', 'fontSize': '24px',
 #                                            'border': '4px solid black', 'font-family': 'Arial'}),
+
+#pattern_shape = "nation", pattern_shape_sequence = [".", "x", "+"]
+
+            # fig = px.bar(df, x="sex", y="total_bill", color="smoker", barmode="group", facet_row="time", facet_col="day",
+            #        category_orders={"day": ["Thur", "Fri", "Sat", "Sun"], "time": ["Lunch", "Dinner"]})
+
+            # fig = px.scatter_matrix(df, dimensions=["sepal_width", "sepal_length", "petal_width", "petal_length"], color="species")
+
+            # fig = px.parallel_categories(df, color="size", color_continuous_scale=px.colors.sequential.Inferno)
+
+            # fig = px.parallel_coordinates(df, color="species_id", labels={"species_id": "Species",
+            #                   "sepal_width": "Sepal Width", "sepal_length": "Sepal Length",
+            #                   "petal_width": "Petal Width", "petal_length": "Petal Length", },
+            #                     color_continuous_scale=px.colors.diverging.Tealrose, color_continuous_midpoint=2)
 
 
 #st.spinner()
