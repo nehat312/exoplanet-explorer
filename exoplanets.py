1798133cdd59a1c2edbd64df58ee4d7cf9160081
nehat312 <nate.ehat@gmail.com>
2022-08-08 17:12:03 -0400

    Update exoplanets.py

--- exoplanets.py
+++ exoplanets.py
@@ -182,10 +182,7 @@ scatter_matrix_1 = px.scatter_matrix(exoplanets,
 
 st.plotly_chart(exo_chart_1, use_container_width=False, sharing="streamlit")
 st.plotly_chart(star_chart_1, use_container_width=False, sharing="streamlit")
-st.plotly_chart(scatter_matrix_1, use_container_width=False, sharing="streamlit")
-
-
-st.plotly_chart((exo_chart_1, star_chart_1), use_container_width=False, sharing="streamlit")
+st.plotly_chart(scatter_matrix_1, use_container_width=True, sharing="streamlit")
 
 
 
