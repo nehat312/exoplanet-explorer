5ed7c8872986ab833436f20edbcb4c9630bed22d
nehat312 <nate.ehat@gmail.com>
2022-08-09 17:21:51 -0400

    Update exoplanets.py

--- exoplanets.py
+++ exoplanets.py
@@ -142,8 +142,8 @@ st.title('EXOPLANET EXPLORER')
 st.subheader('*Sourced from NASA-CalTECH mission archives*')
 
 disc_year_1 = px.scatter(exoplanets,
-                      x=exoplanets['disc_year'].sort_values(ascending=True),
-                      y=exoplanets['sy_distance_pc'],
+                      x=exoplanets['ra'].sort_values(ascending=True),
+                      y=exoplanets['dec'], #['sy_distance_pc'],
                            color=exoplanets['disc_method'],
                            color_discrete_sequence=Temps,
                            hover_name=exoplanets['pl_name'],
