0228ccb16d6990afa6d624d4eca5c72112288b2e
nehat312 <nate.ehat@gmail.com>
2022-08-09 17:13:01 -0400

    Update exoplanets.py

--- exoplanets.py
+++ exoplanets.py
@@ -143,7 +143,7 @@ st.subheader('*Sourced from NASA-CalTECH mission archives*')
 
 disc_year_1 = px.line(exoplanets,
                       x=exoplanets['disc_year'],
-                      y=exoplanets['disc_year'].value_counts().sort_values(ascending=True),
+                      y=exoplanets['disc_year'].sort_values(ascending=True),
                            color=exoplanets['disc_method'],
                            color_discrete_sequence=Temps,
                            hover_name=exoplanets['pl_name'],
