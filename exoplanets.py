9a23f769faa8906038e86d4bf54ee23acbe64f99
nehat312 <nate.ehat@gmail.com>
2022-08-12 22:48:04 -0400

    Update exoplanets.py

--- exoplanets.py
+++ exoplanets.py
@@ -130,12 +130,11 @@ exo_with_dist = exoplanets[['sy_distance_pc']].dropna()
 # print(disc_method_time.disc_year)
 
 disc_facility_filter = exoplanets[exoplanets['facility_count'] > 1]
-print(disc_facility_filter)
 
-#%%
 facility_filtered = disc_facility_filter['disc_facility'].unique()
-print(facility_filtered)
-#%%
+# print(disc_facility_filter)
+# print(facility_filtered)
+
 
 ## VISUALIZATION ##
 scatter_3d_1 = px.scatter_3d(exo_drop_na,
@@ -178,57 +177,6 @@ disc_info_1 = px.histogram(disc_facility_filter,
                            # width=800,
                            )
 
-# disc_year_1 = px.bar(exoplanets,
-#                      # x=exoplanets['disc_year'],
-#                      y=exoplanets['disc_method'],
-#                      color=exoplanets['disc_method'],
-#                      color_discrete_sequence=Temps,
-#                      color_continuous_scale=Temps,
-#                      hover_name=exoplanets['pl_name'],
-#                      hover_data=exoplanets[['host_name', 'disc_telescope', 'disc_facility']],
-#                      # barmode='group',
-#                      # animation_frame=exoplanets['disc_year'],
-#                      title='EXOPLANET DISCOVERY METHOD',
-#                      labels=chart_labels,
-#                      range_x=[0, 4000],
-#                      # range_y=disc_method_list,
-#                      # height=800,
-#                      width=800,
-#                      orientation='h',
-#                      )
-
-
-
-# density_map_1 = px.density_contour(exoplanets,
-#                                    x=exoplanets['ra'],
-#                                    y=exoplanets['dec'],
-#                                    z=exoplanets['sy_distance_pc'],
-#                                    color=exoplanets['disc_method'],
-#                                    color_discrete_sequence=Temps,
-#                                    hover_name=exoplanets['pl_name'],
-#                                    hover_data=exoplanets[['host_name', 'disc_facility', 'disc_telescope', 'sy_star_count', 'sy_planet_count']],
-#                                    title='EXOPLANET RIGHT ASCENSION / DECLINATION',
-#                                    labels=chart_labels,
-#                                    )
-
-
-# exo_map_1 = px.scatter_geo(exoplanets,
-#                         lat=[['glat']],
-#                         lon=[['glon']],
-#                         title='EXOPLANET GLAT / GLON',
-#                         labels=chart_labels,
-#                         )
-
-# exo_select_1 = px.(exoplanet_selection,
-#                                  dimensions=exoplanets['pl_rade', 'pl_bmasse', 'pl_orbper', 'pl_orbeccen'], #, 'pl_orbsmax'
-#                                      # color=exoplanets['st_temp_eff_k'],
-#                                      color_continuous_scale=Temps,
-#                                      color_discrete_sequence=Temps,
-#                                      hover_name=exoplanets['pl_name'],
-#                                      hover_data=exoplanets[['host_name', 'sy_star_count', 'sy_planet_count']],
-#                                      title='EXOPLANET ATTRIBUTES',
-#                                      labels=chart_labels,
-#                                      )
 
 exo_matrix_1 = px.scatter_matrix(exoplanets,
                                      dimensions=['pl_rade', 'pl_bmasse', 'pl_orbper', 'pl_orbeccen'], #, 'pl_orbsmax'
@@ -370,54 +318,11 @@ left_col_1, right_col_1 = st.columns(2)
 left_col_1.plotly_chart(exo_matrix_1, use_container_width=False, sharing="streamlit")
 right_col_1.plotly_chart(star_matrix_1, use_container_width=False, sharing="streamlit")
 
-# st.plotly_chart(density_map_1, use_container_width=False, sharing="streamlit")
 
+# st.plotly_chart(density_map_1, use_container_width=False, sharing="streamlit")
 # st.plotly_chart(exo_scatter_1, use_container_width=False, sharing="streamlit")
 # st.plotly_chart(star_scatter_1, use_container_width=False, sharing="streamlit")
 
-
-## SUBPLOTS ##
-# subplots = make_subplots(rows=1, cols=2)
-    # subplots.add_trace(scatter_3d_1, row=1, col=1)
-    # subplots.add_trace(scatter_3d_1, row=1, col=2)
-
-
-# with st.form('PARAMS FORM'):
-#     if sector == "MULTIFAMILY":
-#         prop_size = st.number_input('*TOTAL MF UNITS [25-1,000 UNITS]', min_value=25, max_value=500, step=25, value=100) #list(range(25,750,25)))
-#         min_prop_price = st.number_input('*MINIMUM VALUATION [$0MM-$100MM]', min_value=0, max_value=100, value=10, step=5)
-#         prop_qual = st.selectbox('*PROPERTY QUALITY [1-5]:', list(range(1, 6, 1)))
-
-
-#     params_submit = st.form_submit_button('FORM SUBMIT')
-
-#     @st.cache(persist=True, allow_output_mutation=True)
-#     def filter_buyers(sector, prop_size, min_prop_price, prop_qual):
-#       if sector == 'MULTIFAMILY':
-#         for investors in all_investor_idx:
-#           mf_size_filter = all_investor_idx[all_investor_idx.MF_UNITS_PROP >= prop_size]
-#           mf_min_price_filter = mf_size_filter[mf_size_filter.MF_AVG_PRICE_MM >= min_prop_price]
-#           mf_qual_filter = mf_min_price_filter[(mf_min_price_filter.MF_QUALITY >= (prop_qual-1)) & (mf_min_price_filter.MF_QUALITY <= (prop_qual+1))]
-#           mf_buyer_recs = mf_qual_filter.sort_values(by = 'MF_VOL_RANK', ascending = True)[:50]
-#           mf_buyer_recs = pd.DataFrame(data = mf_buyer_recs, columns = mf_cols)
-#         return mf_buyer_recs
-#       elif sector == 'STRIP CENTER':
-#         for investors in all_investor_idx:
-#           sc_size_filter = all_investor_idx[all_investor_idx['SC_SF_PROP'] >= prop_size]
-#           sc_min_price_filter = sc_size_filter[sc_size_filter['SC_AVG_PRICE_MM'] >= min_prop_price]
-#           sc_qual_filter = sc_min_price_filter[(sc_min_price_filter['SC_QUALITY'] >= (prop_qual-1)) & (sc_min_price_filter['SC_QUALITY'] <= (prop_qual+1))]
-#           sc_buyer_recs = sc_qual_filter.sort_values(by = 'SC_VOL_RANK', ascending = True)[:50]
-#           sc_buyer_recs = pd.DataFrame(data = sc_buyer_recs, columns = sc_cols)
-#         return sc_buyer_recs
-#
-#
-# ## USER PARAMS DATAFRAME ##
-#     if params_submit:
-#         buyer_rec_df = filter_buyers(sector, prop_size, min_prop_price, prop_qual)
-
-        # st.write("TARGETED INVESTOR POOL:")
-        # st.dataframe(buyer_rec_df)
-            # buyer_rec_df = buyer_rec_df.set_index('INVESTOR')
 
         ## DATAFRAME STYLING ##
 
@@ -432,18 +337,6 @@ right_col_1.plotly_chart(star_matrix_1, use_container_width=False, sharing="stre
 
 
 
-# left_button = left_column.button('GITHUB REPOSITORY')
-# right_button = right_column.button('NASA ARCHIVES')
-# if left_button:
-#     # left_column.write('https://github.com/nehat312/exoplanet-explorer')
-# if right_button:
-    # right_column.write('https://exoplanetarchive.ipac.caltech.edu/')
-
-    # st.markdown(github_link, unsafe_allow_html=True)
-    # st.markdown(nasa_caltech_link, unsafe_allow_html=True)
-
-
-
 ## GALAXY IMAGES ##
 img_col_1, img_col_2, img_col_3 = st.columns(3)
 img_col_1.image(jwst_carina_img_1, caption='CARINA NEBULA (JWST)', width=400)
@@ -454,8 +347,6 @@ img_col_3.image(jwst_infra_img_1, caption='INFRARED PANORAMIC (JWST)', width=400
 ## CONCLUSION ##
 
 st.stop()
-# st.success('')
-# st.warning('')
 
 
 ### INTERPRETATION ###
@@ -473,93 +364,35 @@ st.stop()
 
 ### SCRATCH NOTES
 
-
-## FONTS ##
-
-# t = st.radio("Toggle to see font change", [True, False])
-#
-# if t:
-#     st.markdown(
-#         """
-#         <style>
-# @font-face {
-#   font-family: 'Tangerine';
-#   font-style: normal;
-#   font-weight: 400;
-#   src: url(https://fonts.gstatic.com/s/tangerine/v12/IurY6Y5j_oScZZow4VOxCZZM.woff2) format('woff2');
-#   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-# }
-#
-#     html, body, [class*="css"]  {
-#     font-family: 'Tangerine';
-#     font-size: 48px;
-#     }
-#     </style>
-#
-#     """,
-#         unsafe_allow_html=True,
-#     )
-#
-# "# Hello"
-#
-# """This font will look different, based on your choice of radio button"""
-
-# CONFIG TEMPLATE
-    # st.set_page_config(page_title="CSS hacks", page_icon=":smirk:")
-    #
-    # c1 = st.container()
-    # st.markdown("---")
-    # c2 = st.container()
-    # with c1:
-    #     st.markdown("Hello")
-    #     st.slider("World", 0, 10, key="1")
-    # with c2:
-    #     st.markdown("Hello")
-    #     st.slider("World", 0, 10, key="2")
-
-# STYLE WITH CSS THROUGH MARKDOWN
-    # st.markdown("""
-    # <style>
-    # div[data-testid="stBlock"] {
-    #     padding: 1em 0;
-    #     border: thick double #32a1ce;
-    # }
-    # </style>
-    # """, unsafe_allow_html=True)
+# disc_year_1 = px.bar(exoplanets,
+#                      # x=exoplanets['disc_year'],
+#                      y=exoplanets['disc_method'],
+#                      color=exoplanets['disc_method'],
+#                      color_discrete_sequence=Temps,
+#                      color_continuous_scale=Temps,
+#                      hover_name=exoplanets['pl_name'],
+#                      hover_data=exoplanets[['host_name', 'disc_telescope', 'disc_facility']],
+#                      # barmode='group',
+#                      # animation_frame=exoplanets['disc_year'],
+#                      title='EXOPLANET DISCOVERY METHOD',
+#                      labels=chart_labels,
+#                      range_x=[0, 4000],
+#                      # range_y=disc_method_list,
+#                      # height=800,
+#                      width=800,
+#                      orientation='h',
+#                      )
 
 
-# STYLE WITH JS THROUGH HTML IFRAME
-    # components.html("""
-    # <script>
-    # const elements = window.parent.document.querySelectorAll('div[data-testid="stBlock"]')
-    # console.log(elements)
-    # elements[0].style.backgroundColor = 'paleturquoise'
-    # elements[1].style.backgroundColor = 'lightgreen'
-    # </script>
-    # """, height=0, width=0)
 
-
-# st.markdown("""
-#             <style>
-#             div[data-testid="stBlock"] {padding: 1em 0; border: thick double #32a1ce; color: blue}
-#             </style>
-#             """,
-#             unsafe_allow_html=True)
-
-# style={'textAlign': 'Center', 'backgroundColor': 'rgb(223,187,133)',
-#                                            'color': 'black', 'fontWeight': 'bold', 'fontSize': '24px',
-#                                            'border': '4px solid black', 'font-family': 'Arial'}),
-
-#pattern_shape = "nation", pattern_shape_sequence = [".", "x", "+"]
-
-            # fig = px.bar(df, x="sex", y="total_bill", color="smoker", barmode="group", facet_row="time", facet_col="day",
-            #        category_orders={"day": ["Thur", "Fri", "Sat", "Sun"], "time": ["Lunch", "Dinner"]})
-
-            # fig = px.scatter_matrix(df, dimensions=["sepal_width", "sepal_length", "petal_width", "petal_length"], color="species")
-
-            # fig = px.parallel_categories(df, color="size", color_continuous_scale=px.colors.sequential.Inferno)
-
-            # fig = px.parallel_coordinates(df, color="species_id", labels={"species_id": "Species",
-            #                   "sepal_width": "Sepal Width", "sepal_length": "Sepal Length",
-            #                   "petal_width": "Petal Width", "petal_length": "Petal Length", },
-            #                     color_continuous_scale=px.colors.diverging.Tealrose, color_continuous_midpoint=2)
+# density_map_1 = px.density_contour(exoplanets,
+#                                    x=exoplanets['ra'],
+#                                    y=exoplanets['dec'],
+#                                    z=exoplanets['sy_distance_pc'],
+#                                    color=exoplanets['disc_method'],
+#                                    color_discrete_sequence=Temps,
+#                                    hover_name=exoplanets['pl_name'],
+#                                    hover_data=exoplanets[['host_name', 'disc_facility', 'disc_telescope', 'sy_star_count', 'sy_planet_count']],
+#                                    title='EXOPLANET RIGHT ASCENSION / DECLINATION',
+#                                    labels=chart_labels,
+#                                    )
